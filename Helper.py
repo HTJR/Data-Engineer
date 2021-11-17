8fa2f007eeb56d9f11e004913329a886f788dd52
htjr <hrashikeshtiwari1234@gmail.com>
2021-11-17 19:29:34 +0530

    upd

---
+++ Helper.py
@@ -0,0 +1,38 @@
+import mysql.connector
+import pandas as pd
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Wu@tang1434",
+  database="mysql"
+)
+mycursor = mydb.cursor()
+
+def create_table(nam):
+  sql='''CREATE TABLE  {0} (Customer_Name varchar(255) NOT NULL,Customer_ID varchar(18) NOT NULL,Customer_Open_Date DATE NOT NULL,Last_Consulted_DATE DATE,Vaccination_Type char(5),Doctor_Consulted char(255),State char(5),Country char(5),Date_of_Birth DATE,Active_Customer char(1))'''
+  mycursor.execute(sql.format((nam)))
+  print("Table"+nam+"created")
+
+def insert_table(col,row,tab):
+  sql = "INSERT INTO `" +tab+ "` (`" +col+ "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+  if isinstance(row,pd.Series):
+    mycursor.execute(sql, tuple(row))
+  else:
+    mycursor.execute(sql,row)
+  mydb.commit()
+
+def get_table():
+  mycursor.execute("show tables")
+  tab=[i[0] for i in mycursor]
+  return tab
+
+def get_col():
+  mycursor.execute("select column_name from information_schema.columns where table_name='customer' ORDER BY ORDINAL_POSITION")
+  lis=[i[0] for i in mycursor]
+  return lis
+
+def get_data():
+  sql="SELECT *,country FROM customer"
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  return myresult
---
+++ main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from Helper import create_table,get_data,get_col,get_table,insert_table
+import os
+f= open('Data.txt','r')
+
+header=""
+bod=[]
+
+for i in f:
+    dat=i[1:].strip().split('|')
+    if dat[0]=='H':
+        header=dat[1:]
+    else:
+        bod.append(dat[1:])
+f.close()
+df=pd.DataFrame(data=bod,index=None,columns=header)
+if not os.path.isfile('data.csv'):
+  df.to_csv('data.csv',index=False)
+else:
+  df.to_csv('data.csv',index=False,header=False,mode='a')
+
+
+df['Open_Date']=pd.to_datetime(df['Open_Date'])
+df['Last_Consulted_Date']=pd.to_datetime(df['Last_Consulted_Date'])
+df['DOB']=[i[len(i)-4:]+i[0:len(i)-4] for i in df['DOB'].tolist()]
+df['DOB']= pd.to_datetime(df['DOB'],dayfirst=True)
+
+tab=get_table()
+
+if tab is None:
+  create_table("customer")
+elif "customer" not in tab:
+  create_table("customer")
+else:
+  print("already created")
+
+lis=get_col()
+
+col= "`,`".join([str(i) for i in lis])
+
+for i,row in df.iterrows():
+  insert_table(col,row,"customer")
+
+myresult = get_data()
+for x in myresult:
+  country=x[-1]
+  info=x[0:-1]
+  tab=get_table()
+  cn="table_"+country.lower()
+  if cn not in tab:
+    create_table("Table_"+country)
+    insert_table(col,info,cn)
+  else:
+    insert_table(col,info,cn)
